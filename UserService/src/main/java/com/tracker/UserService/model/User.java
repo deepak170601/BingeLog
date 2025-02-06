b6317974250b66fd71eb2ae84fa7bc44b944974c
Deepak Chekurthi(UST <286135@ust.com>
2025-02-06 11:41:38 +0530

    supabase fixes

--- UserService/src/main/java/com/tracker/UserService/model/User.java
+++ UserService/src/main/java/com/tracker/UserService/model/User.java
@@ -39,57 +39,29 @@ public class User implements UserDetails {
         return List.of();
     }
 
-    /**
-     * Returns the password used to authenticate the user.
-     *
-     * @return the password
-     */
     @Override
     public String getPassword() {
         return password;
     }
 
-    /**
-     * Returns the username used to authenticate the user. In this case, we use email as the username.
-     *
-     * @return the email
-     */
     @Override
     public String getUsername() {
         return email;
     }
 
-    /**
-     * Indicates whether the user's account has expired. An expired account cannot be authenticated.
-     *
-     * @return true if the user's account is valid (i.e., non-expired), false otherwise
-     */
     @Override
     public boolean isAccountNonExpired() {
-        // Implement your logic here. For simplicity, we'll assume accounts never expire.
         return true;
     }
 
-    /**
-     * Indicates whether the user is locked or unlocked. A locked user cannot be authenticated.
-     *
-     * @return true if the user is not locked, false otherwise
-     */
+
     @Override
     public boolean isAccountNonLocked() {
-        // Implement your logic here. For example, you can add a field like 'isLocked' to the User entity.
-        // For simplicity, we'll assume accounts are never locked.
         return true;
     }
 
-    /**
-     * Indicates whether the user's credentials (password) has expired. Expired credentials prevent authentication.
-     *
-     * @return true if the user's credentials are valid (i.e., non-expired), false otherwise
-     */
     @Override
     public boolean isCredentialsNonExpired() {
-        // Implement your logic here. For simplicity, we'll assume credentials never expire.
         return true;
     }
 
@@ -97,7 +69,5 @@ public class User implements UserDetails {
     public boolean isEnabled() {
         return UserDetails.super.isEnabled();
     }
-
-
 
 }
